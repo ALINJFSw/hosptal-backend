da17978feb8bf37604dba245dfd88944baa3a84f
AlINJFSw <alijanbeincs@gmail.com>
2023-03-10 22:50:00 +0200

    added test post date

--- register_api.php
+++ register_api.php
@@ -1,3 +1,45 @@
 <?php
 
-include("connection.php")
+include("connection.php");
+
+$response = [];
+
+if(isset($_POST["patient"])) {
+    if(isset(
+        $_POST["email"])
+        && $_POST["password"]
+        && $_POST["name"]
+        && $_POST["dob"]
+        && $_POST["blood_type"]
+        && $_POST["ehr"]
+        )
+        {$email = $_POST["email"];
+        $password = $_POST["password"];
+        $name = $_POST["name"];
+        $dob = $_POST["dob"];
+        $blood_type = $_POST["blood_type"];
+        $ehr = $_POST["ehr"];}
+}
+else if (isset($_POST["employee"])){
+    if(isset(
+        $_POST["email"])
+        && $_POST["password"]
+        && $_POST["name"]
+        && $_POST["dob"]
+        && $_POST["ssn"]
+        && $_POST["date_joined"]
+        && $_POST["position"]
+        )
+        {
+            $email = $_POST["email"];
+            $password = $_POST["password"];
+            $name = $_POST["name"];
+            $dob = $_POST["dob"];
+            $date_joined = $_Post["date_joined"];
+            $position = $_Post["position"];
+            $ssn = $_Post["ssn"];
+
+        }
+}
+
+    
