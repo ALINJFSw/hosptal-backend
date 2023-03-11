ea98e8dbc687f34a639689f01b0bf6ed4814a468
AlINJFSw <alijanbeincs@gmail.com>
2023-03-11 19:20:46 +0200

    added admin assign patient and emplyee

--- assign_employee_hospital_api.php
+++ assign_employee_hospital_api.php
@@ -13,31 +13,42 @@ if (
     $email = $_POST["email"];
     $hospital = $_POST["hospital"];
     $hos_info = getHospitalInfoByName($hospital);
-    $patien_info = getUserInfoByName($email);
+    $user_info = getUserInfoByName($email);
     $is_active = "true";
     $dj = "today";
-    print($data["user_type"]);
+
+    if ($user_info["user_type"] == "patient") {
+        $query = $mysqli->prepare('select * from hospital_users where user_id = ?');
+        $query->bind_param('i', $user_info["user_id"]);
+        $query->execute();
+
+        $query->store_result();
+        $num_rows = $query->num_rows();
+        if ($num_rows > 0) {
+            $response["result"] = "user can be assigned to many hospital";
+            echo json_encode($response);
+            die();
+
+        }
+    }
+
     if ($data["user_type"] == "admin") {
         $sql = "insert into hospital_users (hospital_id, user_id, is_active, date_joined ) values (?,?,?,?)";
         $query = $mysqli->prepare($sql);
-        $query->bind_param("iiss", $hos_info["hospital_id"],$patien_info["user_id"],$is_active,$dj);
-        $result = $query -> execute();
-        if($result){
+        $query->bind_param("iiss", $hos_info["hospital_id"], $user_info["user_id"], $is_active, $dj);
+        $result = $query->execute();
+        if ($result) {
             $response["result"] = "user assigned to hospital";
 
-        }
-        else {
+        } else {
             $response["result"] = "can't assign to hospital";
 
         }
-    }
-    else {
+    } else {
         $response["result"] = "can't add";
 
     }
-}
-
-else {
+} else {
     $response["result"] = "error";
 }
 
